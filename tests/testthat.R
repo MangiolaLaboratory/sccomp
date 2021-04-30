bdc795fafef344b4d11ff3949564772ee5e0b8bf
stemangiola <mangiolastefano@gmail.com>
2021-04-30 18:05:32 +1000

    added estimates in the results

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,11 @@
-library(testthat)
-library(ppcseq)
+
 
 test_check("ppcseq")
+library(testthat)
+library(sccomp)
+res =
+  sccomp::cell_counts %>%
+  sccomp_glm(
+    formula = ~ type,
+    sample, cell_type, count
+  )
