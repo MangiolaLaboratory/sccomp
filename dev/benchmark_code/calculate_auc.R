97ce973848070bfbeb9f390658a9b2734a2da1de
stemangiola <mangiolastefano@gmail.com>
2021-08-12 17:28:31 +1000

    get threshold at 0.1, and add random classifier

--- dev/benchmark_code/calculate_auc.R
+++ dev/benchmark_code/calculate_auc.R
@@ -10,15 +10,27 @@ args = commandArgs(trailingOnly=TRUE)
 input_file = args[1]
 output_file = args[2]
 
-readRDS(input_file) %>%
+input = readRDS(input_file)
 
+input %>%
+
+  # Add random
+  bind_rows(
+     input %>%
+       slice(1) %>%
+       mutate(name = "random") %>%
+       mutate(df_for_roc  = map(df_for_roc, ~ .x %>% mutate(probability = runif(n(), 0, 1))))
+  ) %>%
+
+  # Calculate AUC
   mutate(auc = map_dbl(
     df_for_roc,
     ~ .x %>%
-      roc_auc(event_level = 'second', truth = significant, probability) %>%
+      roc_curve(event_level = 'second', truth = significant, probability) %>%
+      filter(1-specificity<=0.1) %>%
+      summarise(.estimate = bayestestR::auc(1-specificity , sensitivity)) %>%
       pull(.estimate)
   )) %>%
   select(-df_for_roc) %>%
+
   saveRDS(output_file)
-
-
