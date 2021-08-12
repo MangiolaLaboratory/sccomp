e8b5662e32b93cfa0baa1fd29251a379466a1a60
stemangiola <mangiolastefano@gmail.com>
2021-08-12 15:56:46 +1000

    calculate the ROV properly

--- dev/benchmark_code/calculate_auc.R
+++ dev/benchmark_code/calculate_auc.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(magrittr)
 library(bayestestR)
+library(yardstick)
+
 
 # Read arguments
 args = commandArgs(trailingOnly=TRUE)
@@ -9,13 +11,14 @@ input_file = args[1]
 output_file = args[2]
 
 readRDS(input_file) %>%
-  group_by(name,  slope, n_samples , n_cell_type,  max_cell_counts_per_sample , add_outliers, probs) %>%
-  summarise(mean_FP_rate = mean(FP_rate), mean_TP_rate = mean(TP_rate)) %>%
-  ungroup() %>%
-  arrange(mean_FP_rate) %>%
-  nest(data = -c(name,  slope, n_samples , n_cell_type,  max_cell_counts_per_sample , add_outliers)) %>%
-  mutate(auc = map_dbl(data, ~ bayestestR::auc(.x$mean_FP_rate , .x$mean_TP_rate))) %>%
-  select(-data) %>%
+
+  mutate(auc = map_dbl(
+    df_for_roc,
+    ~ .x %>%
+      roc_auc(event_level = 'second', truth = significant, probability) %>%
+      pull(.estimate)
+  )) %>%
+  select(-df_for_roc) %>%
   saveRDS(output_file)
 
 
