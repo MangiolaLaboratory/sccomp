8e6ad90f78ae2fc95c65680fd5c015e98f32d024
stemangiola <mangiolastefano@gmail.com>
2021-09-21 18:09:35 +1000

    update devs for qq plot

---
+++ dev/data_integration/estimate_datasets_and_save.R
@@ -0,0 +1,106 @@
+library(tidyverse)
+library(tidyseurat)
+library(sccomp)
+library(job)
+library(patchwork)
+
+
+
+job({
+
+  load("data/counts_obj.rda")
+
+  counts_obj %>%
+    group_by(sample) %>%
+    mutate(proportion = (count+1)/sum(count+1)) %>%
+    ungroup(sample) %>%
+    mutate(proportion_logit = boot::logit(proportion)) %>%
+    group_by(cell_group) %>%
+    summarise(mean = mean(proportion_logit), variance = sd(proportion_logit)) %>%
+    ggplot(aes(mean, variance)) +
+    geom_point() +
+    geom_smooth(method="lm") +
+    theme_bw()
+
+
+    counts_obj  |>
+    mutate(is_benign = type=="benign") |>
+    sccomp_glm(
+      formula = ~ is_benign,
+      sample, cell_group, count,
+      approximate_posterior_inference = FALSE,
+      variance_association = TRUE,
+      prior_mean_variable_association = list(intercept = c(0, 5), slope = c(0,  5), standard_deviation = c(0, 2))
+    ) %>%
+      saveRDS("dev/data_integration/estimate_GSE115189_SCP345_SCP424_SCP591_SRR11038995_SRR7244582_10x6K_10x8K.rds")
+})
+
+job({
+  estimate_UVM =
+    readRDS("dev/data_integration/UVM_single_cell/counts.rds")  |>
+    rename(type = `Sample Type`) %>%
+    sccomp_glm(
+      formula = ~ type,
+      sample, cell_type,
+      approximate_posterior_inference = FALSE,
+      variance_association = TRUE,
+      prior_mean_variable_association = list(intercept = c(0, 5), slope = c(0,  5), standard_deviation = c(0, 2))
+    ) %>%
+    saveRDS("dev/data_integration/estimate_GSE139829_uveal_melanoma.rds")
+
+})
+
+job({
+  estimate_renal_cell_carcinoma =
+    readRDS("dev/data_integration/SCP1288_renal_cell_carcinoma.rds")  |>
+    tidyseurat::filter(!is.na(sample) & !is.na(cell_type) & !is.na(sex))  |>
+    sccomp_glm(
+      formula = ~ sex,
+      sample, cell_type,
+      approximate_posterior_inference = FALSE,
+      variance_association = TRUE,
+      prior_mean_variable_association = list(intercept = c(0, 5), slope = c(0,  5), standard_deviation = c(0, 2))
+    ) %>%
+    saveRDS("dev/data_integration/estimate_SCP1288_renal_cell_carcinoma.rds")
+})
+
+job({
+  estimate_bc_cells =
+    readRDS("dev/data_integration/SCP1039_bc_cells.rds")  |>
+    mutate(type = subtype=="TNBC") %>%
+    sccomp_glm(
+      formula = ~ type,
+      sample, cell_type,
+      approximate_posterior_inference = FALSE,
+      variance_association = TRUE,
+      prior_mean_variable_association = list(intercept = c(0, 5), slope = c(0,  5), standard_deviation = c(0, 2))
+    ) %>%
+    saveRDS("dev/data_integration/estimate_SCP1039_bc_cells.rds")
+})
+
+job({
+  estimate_COVID =
+    readRDS("dev/data_integration/s41587-020-0602-4_COVID_19.rds")  |>
+    mutate(is_critical = severity=="critical") %>%
+    sccomp_glm(
+      formula = ~ is_critical,
+      sample, cell_type,
+      approximate_posterior_inference = FALSE,
+      variance_association = TRUE,
+      prior_mean_variable_association = list(intercept = c(0, 5), slope = c(0,  5), standard_deviation = c(0, 2))
+    ) %>%
+    saveRDS("dev/data_integration/estimate_s41587-020-0602-4_COVID_19.rds")
+})
+
+job({
+  estimate_melanoma =
+    readRDS("dev/data_integration/GSE120575_melanoma.rds")  |>
+    sccomp_glm(
+      formula = ~ time,
+      sample, cell_type,
+      approximate_posterior_inference = FALSE,
+      variance_association = TRUE,
+      prior_mean_variable_association = list(intercept = c(0, 5), slope = c(0,  5), standard_deviation = c(0, 2))
+    ) %>%
+    saveRDS("dev/data_integration/estimate_GSE120575_melanoma.rds")
+})
---
+++ dev/data_integration/simulation_qq_plots.R
@@ -0,0 +1,118 @@
+# simulation QQ plots
+
+library(tidyverse)
+library(tidyseurat)
+library(sccomp)
+library(job)
+library(patchwork)
+
+multipanel_theme =
+  theme_bw() +
+  theme(
+    panel.border = element_blank(),
+    axis.line = element_line(size=0.5),
+    panel.grid.major = element_line(size = 0.2),
+    panel.grid.minor = element_line(size = 0.1),
+    legend.position = "bottom",
+    strip.background = element_blank(),
+    axis.title.y = element_text(margin = margin(t = 0, r = 0, b = 0, l = 0), size = 7),
+    axis.title.x = element_text(margin = margin(t = 0, r = 0, b = 0, l = 0), size = 7),
+    panel.spacing.x=unit(0.1, "lines"),
+    axis.text.x = element_text(size=6),
+    axis.text.y = element_text(size=6),
+    strip.text.x = element_text(size = 7),
+    strip.text.y = element_text(size = 7)
+  )
+
+data_for_plot =
+
+  # Import data
+  tribble(
+    ~dataset, ~data,
+    "oligo",
+    readRDS("dev/data_integration/estimate_GSE115189_SCP345_SCP424_SCP591_SRR11038995_SRR7244582_10x6K_10x8K.rds") %>%
+      rename(cell_type = cell_group),
+    "UVM",
+    readRDS("dev/data_integration/estimate_GSE139829_uveal_melanoma.rds"),
+    "renal_cell_carcinoma",
+    readRDS("dev/data_integration/estimate_SCP1288_renal_cell_carcinoma.rds"),
+    "bc_cells",
+    readRDS("dev/data_integration/estimate_SCP1039_bc_cells.rds") %>%
+      mutate(count_data  = map(count_data , ~mutate(.x, type = as.character(type)))),
+    "COVID",
+    readRDS("dev/data_integration/estimate_s41587-020-0602-4_COVID_19.rds"),
+    "melanoma",
+    readRDS("dev/data_integration/estimate_GSE120575_melanoma.rds")
+  ) %>%
+
+    # Simulate
+    mutate(simulation = map(
+      data,
+      ~  .x %>%
+        simulate_data(sample, cell_type) %>%
+        unnest(generated_data) %>%
+        with_groups(
+          cell_type,
+          ~.x %>%
+            arrange(generated_proportions ) %>%
+            mutate(sample = 1:n() )
+        )
+    )) %>%
+
+    # Calculate proportion
+    mutate(data = map(
+      data,
+      ~ .x %>%
+        unnest(count_data) %>%
+        select(cell_type, sample, outlier, count) %>%
+        with_groups(sample, ~ mutate(.x, proportion = (count)/sum(count)) ) %>%
+        with_groups(cell_type, ~.x %>% arrange(proportion) %>%  mutate(sample = 1:n() ))
+    )) %>%
+
+    # Join data
+    mutate(data = map2(
+      data, simulation,
+      ~ left_join(.x, .y, by = c("cell_type", "sample") )
+    )) %>%
+    select(-simulation) %>%
+
+
+    # linear model for qq plot
+    unnest(data) %>%
+    filter(!outlier) %>%
+    nest(data = -c(dataset, cell_type)) %>%
+    mutate(slope = map_dbl(
+      data,
+      ~ lm(generated_proportions~proportion, data=.x) %>%
+        broom::tidy() %>%
+        filter(term!="(Intercept)") %>%
+        pull(estimate )
+    ))
+
+# Import data
+
+
+plot_qq =
+  data_for_plot %>%
+  unnest(data) %>%
+  ggplot(aes(proportion, generated_proportions, group=cell_type)) +
+  geom_point(size=0.2, alpha=0.5) +
+  geom_smooth(aes(color=dataset), method = "lm", se = FALSE, size=0.2, alpha=0.5) +
+  facet_wrap(~dataset) +
+  geom_abline(linetype="dashed", color="grey") +
+  scale_color_brewer(palette="Set1") +
+  scale_x_continuous(trans="logit") +
+  scale_y_continuous(trans="logit") +
+  multipanel_theme
+
+plot_slopes =
+  data_for_plot  %>%
+  ggplot(aes(slope, color=dataset))+
+  geom_density() +
+  scale_color_brewer(palette="Set1") +
+  scale_x_log10() +
+  multipanel_theme
+
+( plot_qq / plot_slopes ) +
+  plot_layout(guides = 'collect', ) &
+  theme( plot.margin = margin(0, 0, 0, 0, "pt"),  legend.key.size = unit(0.2, 'cm'), legend.position = "bottom")
