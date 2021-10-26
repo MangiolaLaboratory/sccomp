0e2a51268d5c7ba5218d5907c9548f53954e7813
stemangiola <mangiolastefano@gmail.com>
2021-10-26 17:15:50 +1100

    update benchmark with rlm with proper convergence

--- dev/article_figures/benchmark.R
+++ dev/article_figures/benchmark.R
@@ -6,13 +6,18 @@ library(glue)
 library(purrr)
 
 # Load multipanel_theme
-source("https://gist.githubusercontent.com/stemangiola/fc67b08101df7d550683a5100106561c/raw/305ed9ba2af815fdef3214b9e6171008d5917400/ggplot_theme_multipanel")
+source("https://gist.githubusercontent.com/stemangiola/fc67b08101df7d550683a5100106561c/raw/7a6df4a80d38b0427a263c21ac2480c4280cfe4b/ggplot_theme_multipanel")
 
 set.seed(42)
 
 result_directory = "dev/benchmark_results_0568ae31db577b7a39976a96327e2ac741247c77"
 
 custom_paired_palette = c("#E31A1C" ,"#A6CEE3" ,"#1F78B4", "#B2DF8A", "#33A02C", "#FB9A99","#FDBF6F", "#FF7F00", "#CAB2D6", "#6A3D9A", "#FFFF99", "#B15928")
+
+cool_palette = c("#b58b4c", "#74a6aa", "#a15259",  "#37666a", "#79477c", "#cb9f93", "#9bd18e", "#eece97", "#8f7b63", "#4c474b", "#415346")
+color_palette_link = cool_palette %>% setNames(c("Diff abundant_FALSE", "Diff heterogeneous_FALSE", "Non-significant_TRUE", "Diff abundant_TRUE", "Non-significant_FALSE"))
+scales::show_col(cool_palette)
+
 
 plot_auc =
   dir(result_directory, pattern = "auc", full.names = TRUE) %>%
@@ -39,7 +44,7 @@ plot_auc =
       facet_grid( fct_reorder(n_cell_type_,n_cell_type)  ~ fct_reorder(n_samples_, n_samples), scale="free_y") +
       geom_hline(yintercept = 0.1, linetype="dashed", color="grey") +
       scale_y_continuous( labels = dropLeadingZero) +
-      scale_color_manual(values = friendly_cols) +
+      scale_color_manual(values = cool_palette) +
       theme_bw() +
       ylab("Accuracy (AUC) up to 0.1 false-positive-rate") +
       xlab("Slope") +
@@ -65,7 +70,7 @@ plot_ROC =
   geom_rect( mapping=aes(xmin=0, xmax=0.1, ymin=0, ymax=1), fill="#EBECF0", color=NA, alpha=0.1) +
   geom_path(size=0.3) +
   geom_abline(lty = 3) +
-  scale_color_manual(values = friendly_cols) +
+  scale_color_manual(values = cool_palette) +
   scale_y_continuous( labels = dropLeadingZero) +
   guides(color="none") +
   multipanel_theme +
