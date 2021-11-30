8a5c67d02e37383064d51a666e61832a3e922b18
stemangiola <mangiolastefano@gmail.com>
2021-12-01 10:03:16 +1100

    fix simulation for Castiel

--- R/utilities.R
+++ R/utilities.R
@@ -789,7 +789,7 @@ parse_generated_quantities = function(rng, number_of_draws = 1){
 #'
 #'
 design_matrix_and_coefficients_to_simulation = function(
-  design_matrix, coefficient_matrix
+  design_matrix, coefficient_matrix, .estimate_object
 ){
 
   design_df = as.data.frame(design_matrix)
@@ -810,6 +810,7 @@ design_matrix_and_coefficients_to_simulation = function(
     left_join(coefficient_df |>as_tibble(rownames = "cell_type"), by = "cell_type")
 
   simulate_data(.data = input_data,
+                .estimate_object = .estimate_object,
                 formula = ~ covariate_1 ,
                 .sample = sample,
                 .cell_group = cell_type,
