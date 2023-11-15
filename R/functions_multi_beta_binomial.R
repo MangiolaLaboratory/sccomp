3bbc11819acf8405c29ae2e5a577eebee57a9ff7
Stefano Mangiola <mangiolastefano@gmail.com>
2023-11-15 19:38:13 +1100

    add changes in master

--- R/functions_multi_beta_binomial.R
+++ R/functions_multi_beta_binomial.R
@@ -53,24 +53,11 @@ sccomp_glm_data_frame_raw = function(.data,
   
   # Make counts
   .data %>%
+
     class_list_to_counts(!!.sample, !!.cell_group) %>%
     
     # Add formula_composition information
-    when(
-      length(parse_formula(formula_composition))>0 ~
-        left_join(.,
-                  .data %>%
-                    as_tibble() |>
-                    select(
-                      !!.sample,
-                      parse_formula(formula_composition),
-                      any_of(.grouping_for_random_intercept)
-                    ) %>%
-                    distinct(),
-                  by = quo_name(.sample)
-        ),
-      ~ (.)
-    ) |>
+    add_formula_columns(.data, !!.sample,formula_composition) |>
     
     # Attach possible exclusion of data points
     left_join(.data %>%
@@ -161,6 +148,31 @@ sccomp_glm_data_frame_counts = function(.data,
     quo_name(.count),
     parse_formula(formula_composition)
   ))
+  
+  # Check that I have rectangular data frame
+  if(
+    .data |> count(!!.sample) |> distinct(n) |> nrow() > 1 || 
+    .data |> count(!!.cell_group) |> distinct(n) |> nrow() > 1 
+  ){
+    warning(sprintf("sccomp says: the input data frame does not have the same number of `%s`, for all `%s`. We have made it so, adding 0s for the missing sample/feature pairs.", quo_name(.cell_group), quo_name(.sample)))
+    .data = .data |> 
+      
+      # I need renaming trick because complete list(...) cannot accept quosures
+      select(!!.sample, !!.cell_group, count := !!.count) |> 
+      distinct() |> 
+      complete( !!.sample, !!.cell_group, fill = list(count = 0) ) %>%
+      rename(!!.count := count) |> 
+      mutate(!!.count := as.integer(!!.count)) |> 
+      
+      # Add formula_composition information
+      add_formula_columns(.data, !!.sample, formula_composition) 
+  }
+  
+  
+  # Check if test_composition_above_logit_fold_change is 0, as the Bayesian FDR does not allow it
+  if(test_composition_above_logit_fold_change <= 0)
+    stop("sccomp says: test_composition_above_logit_fold_change should be > 0 for the FDR to be calculated in the Bayesian context (doi: 10.1093/biostatistics/kxw041). Also, testing for > 0 differences avoids significant but meaningless (because of the small magnitude) estimates.")
+  
   
   # Check if any column is NA or null
   check_if_any_NA(.data, c(
