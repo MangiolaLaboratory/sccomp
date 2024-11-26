dc2921c9f7977f0e44cc9b709a80b259d5300ccc
Stefano Mangiola <mangiolastefano@gmail.com>
2024-11-26 22:08:49 +1030

    fix WARNINGS

--- tests/testthat/helper-utils.R
+++ tests/testthat/helper-utils.R
@@ -1,17 +1,3 @@
-stan_test <- function(label, code) {
-  skip_if_not_installed("withr")
-  expr <- substitute(
-    testthat::test_that(label, code),
-    env = list(label = label, code = substitute(code))
-  )
-  temp <- tempfile()
-  dir.create(temp)
-  on.exit(unlink(temp))
-  withr::with_dir(
-    temp,
-    suppressMessages(eval(expr, envir = parent.frame()))
-  )
-}
 
 skip_cmdstan <- function() {
   skip_on_cran()
