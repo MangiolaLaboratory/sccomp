0ebd1717959ab7589b195e50159507b77618dfe9
Stefano Mangiola <mangiolastefano@gmail.com>
2024-11-29 14:00:41 +1030

    avoid conversion to quo_names

--- R/functions_multi_beta_binomial.R
+++ R/functions_multi_beta_binomial.R
@@ -39,16 +39,16 @@ sccomp_glm_data_frame_raw = function(.data,
   
   # Check if columns exist
   check_columns_exist(.data, c(
-    quo_name(.sample),
-    quo_name(.cell_group),
-    parse_formula(formula_composition)
+    !!.sample,
+    !!.cell_group,
+    all_of(parse_formula(formula_composition))
   ))
   
   # Check if any column is NA or null
   check_if_any_NA(.data, c(
-    quo_name(.sample),
-    quo_name(.cell_group),
-    parse_formula(formula_composition)
+    !!.sample,
+    !!.cell_group,
+    all_of(parse_formula(formula_composition))
   ))
   
   .grouping_for_random_effect = parse_formula_random_effect(formula_composition) |> pull(grouping) |> unique()
@@ -150,10 +150,11 @@ sccomp_glm_data_frame_counts = function(.data,
   
   # Check if columns exist
   check_columns_exist(.data, c(
-    quo_name(.sample),
-    quo_name(.cell_group),
-    quo_name(.count),
-    parse_formula(formula_composition)
+    !!.sample,
+    !!.cell_group,
+    !!.count,
+    all_of(parse_formula(formula_composition))
+    
   ))
   
   # Check that I have rectangular data frame
--- R/utilities.R
+++ R/utilities.R
@@ -1769,6 +1769,7 @@ design_matrix_and_coefficients_to_simulation = function(
 #' @importFrom rlang ensym
 #' @noRd
 class_list_to_counts = function(.data, .sample, .cell_group){
+
   
   .sample_for_tidyr = ensym(.sample)
   .cell_group_for_tidyr = ensym(.cell_group)
@@ -1776,6 +1777,7 @@ class_list_to_counts = function(.data, .sample, .cell_group){
   .sample = enquo(.sample)
   .cell_group = enquo(.cell_group)
   
+
   .data %>%
     count(!!.sample,
           !!.cell_group,
