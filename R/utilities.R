1f7a7bf990236dd04fe2f3a6a3f22d40ebb7e6bd
Stefano Mangiola <mangiolastefano@gmail.com>
2024-11-29 18:41:53 +1030

    use characters as column names

--- R/utilities.R
+++ R/utilities.R
@@ -1798,7 +1798,6 @@ class_list_to_counts = function(.data, .sample, .cell_group){
   .sample = enquo(.sample)
   .cell_group = enquo(.cell_group)
   
-
   .data %>%
     count(!!.sample,
           !!.cell_group,
