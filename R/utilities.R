fa4ff6feb1a009bb668cdee9580c338345420782
Stefano Mangiola <mangiolastefano@gmail.com>
2024-11-29 14:00:41 +1030

    avoid conversion to quo_names

--- R/utilities.R
+++ R/utilities.R
@@ -1792,6 +1792,10 @@ design_matrix_and_coefficients_to_simulation = function(
 #' @noRd
 class_list_to_counts = function(.data, .sample, .cell_group){
 
+<<<<<<< HEAD
+=======
+  
+>>>>>>> ac6e0ee (avoid conversion to quo_names)
   .sample_for_tidyr = ensym(.sample)
   .cell_group_for_tidyr = ensym(.cell_group)
   
