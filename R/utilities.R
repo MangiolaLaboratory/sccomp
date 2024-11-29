3aa1d3bc8f74dfe440aff86a4c8fdf32b524d95b
Stefano Mangiola <mangiolastefano@gmail.com>
2024-11-29 18:41:53 +1030

    use characters as column names

--- R/utilities.R
+++ R/utilities.R
@@ -1776,19 +1776,17 @@ design_matrix_and_coefficients_to_simulation = function(
 #' @noRd
 class_list_to_counts = function(.data, .sample, .cell_group){
 
+  .sample_for_tidyr = ensym(.sample)
+  .cell_group_for_tidyr = ensym(.cell_group)
+  
   .sample = enquo(.sample)
   .cell_group = enquo(.cell_group)
   
-  
-  .sample_for_tidyr = ensym(.sample)
-  .cell_group_for_tidyr = ensym(.cell_group)
-
-
   .data %>%
     count(!!.sample,
           !!.cell_group,
           name = "count") %>%
-
+    
     complete(
       !!.sample_for_tidyr,!!.cell_group_for_tidyr,
       fill = list(count = 0)
