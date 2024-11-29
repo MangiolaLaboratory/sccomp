ac6e0ee59a206a8e93047beb91e2e5ec683e1815
Stefano Mangiola <mangiolastefano@gmail.com>
2024-11-29 14:00:41 +1030

    avoid conversion to quo_names

--- R/utilities.R
+++ R/utilities.R
@@ -1779,6 +1779,7 @@ design_matrix_and_coefficients_to_simulation = function(
 #' @importFrom rlang ensym
 #' @noRd
 class_list_to_counts = function(.data, .sample, .cell_group){
+
   
   .sample_for_tidyr = ensym(.sample)
   .cell_group_for_tidyr = ensym(.cell_group)
@@ -1786,6 +1787,7 @@ class_list_to_counts = function(.data, .sample, .cell_group){
   .sample = enquo(.sample)
   .cell_group = enquo(.cell_group)
   
+
   .data %>%
     count(!!.sample,
           !!.cell_group,
