ad6a2e8390e10ef5552da0e9e1fffc6bd1c92a96
Stefano Mangiola <mangiolastefano@gmail.com>
2026-03-31 14:43:05 +1100

    Enhance model fitting by introducing conditional initialization for intercept parameters based on design specifications. Update Stan model to reflect intercept-centered priors for the first column and zero-centered priors for others, improving flexibility in handling variability associations.

--- R/model_fitting.R
+++ R/model_fitting.R
@@ -54,13 +54,21 @@ fit_model = function(
 
   # Mean-variability regression: prec_coeff[1|4,a] ~ student_t(3, 4, 2), prec_coeff[2|3,a] ~ student_t(3, 0, 2).
   # Intercepts are modelled as prec_intercept_1 (always) and prec_intercept_2 (bimodal only).
+  has_variability_intercept <- isTRUE(as.logical(data_for_model$intercept_in_design))
+  if (has_variability_intercept && data_for_model$A > 1) {
+    init_prec_intercept_1 <- c(4, rep(0, data_for_model$A - 1))
+    init_prec_intercept_2 <- c(5, rep(0, data_for_model$A - 1))
+  } else {
+    init_prec_intercept_1 <- rep(4, data_for_model$A)
+    init_prec_intercept_2 <- rep(5, data_for_model$A)
+  }
   if (data_for_model$bimodal_mean_variability_association == 1) {
-    init_list$prec_intercept_1 = rep(3, data_for_model$A)
-    init_list$prec_intercept_2 = rep(5, data_for_model$A)
+    init_list$prec_intercept_1 = init_prec_intercept_1
+    init_list$prec_intercept_2 = init_prec_intercept_2
     init_list$prec_slope_1 = rep(0, data_for_model$A)
     init_list$prec_slope_2 = rep(0, data_for_model$A)
   } else {
-    init_list$prec_intercept_1 = rep(4, data_for_model$A)
+    init_list$prec_intercept_1 = init_prec_intercept_1
     init_list$prec_slope_1 = rep(0, data_for_model$A)
   }
 
