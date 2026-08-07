dd440618b2f04ce11ff3d31f7d06806bfcc07530
Stefano Mangiola <mangiolastefano@gmail.com>
2026-08-07 13:04:20 +1000

    update comment

--- R/model_fitting.R
+++ R/model_fitting.R
@@ -69,7 +69,7 @@ fit_model = function(
     init_list$prec_slope_2 = rep(0, data_for_model$A)
   }
 
-  # Random effect inits - 4 uniform slots (one per non-empty random-effect block).
+  # Random effect inits - 5 uniform slots (one per non-empty random-effect block).
   # Each slot gets zero-initialised raws + an identity-like correlation Cholesky.
   if (data_for_model$n_random_eff > 0) {
     init_list$zero_random_effect = rep(0, size = 1) |> as.array()
